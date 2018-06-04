f0720adac28fac87e02409836cb3588c461167d2
offy284 <rod.j.myers@gmail.com>
2018-06-04 15:47:33 -0400

    Maybe rect

--- crumbs/src/lfb.rs
+++ crumbs/src/lfb.rs
@@ -167,7 +167,7 @@ impl Lfb {
     pub fn rect(&self, x: u32, y: u32, width: u32, length: u32) {
         for curr_y in y .. (y + length) {
             for curr_x in x .. (x + width) {
-                let curr_mem_loc = (curr_y * self.lfb.pitch) + (curr_x * 4);
+                let curr_mem_loc = (curr_y * self.pitch) + (curr_x * 4);
                 unsafe { *self.lfb.offset(curr_mem_loc as isize) = WHITE_PIXEL };
             }
         }
