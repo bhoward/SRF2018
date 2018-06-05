f3ff7b07ce51be963b57558516d675e71e2e8b79
offy284 <rod.j.myers@gmail.com>
2018-06-05 15:26:45 -0400

    FIxed colors.rs

---
+++ crumbs/src/colors.rs
@@ -0,0 +1,5 @@
+pub const BLACK_PIXEL: u32 = 0x0000_0000;
+pub const WHITE_PIXEL: u32 = 0x00FF_FFFF;
+pub const RED_PIXEL: u32 =   0x00FF_0000;
+pub const GREEN_PIXEL: u32 = 0x0000_FF00;
+pub const BLUE_PIXEL: u32 =  0x0000_00FF;
