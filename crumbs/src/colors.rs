e88c30242661af282b9d304597d192cc49e6d461
offy284 <rod.j.myers@gmail.com>
2018-06-06 14:14:21 -0400

    This should do some basic window stuff

--- crumbs/src/colors.rs
+++ crumbs/src/colors.rs
@@ -5,4 +5,4 @@ pub const GREEN_PIXEL: u32 = 0x0000_FF00;
 pub const BLUE_PIXEL: u32 =  0x0000_00FF;
 
 pub const TITLE_BAR_COLOR: u32 = 0x00444444;
-pub const BG_COLOR: u32: = 0x004286F4;
+pub const BG_COLOR: u32 = 0x004286F4;
