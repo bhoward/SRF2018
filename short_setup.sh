a57e204d7637a4df3e5942237556d5720ef22cab
Brian Howard <bhoward@depauw.edu>
2018-06-29 16:24:21 -0400

    Updated build to use cargo's llvm binutils

--- short_setup.sh
+++ short_setup.sh
@@ -1,20 +1,6 @@
 rustup default nightly
-rustup component add rust-src
-cargo install xargo
-mkdir src
-cd src
-wget http://ftpmirror.gnu.org/binutils/binutils-2.30.tar.gz
-tar -xzf binutils-2.30.tar.gz 
-rm binutils-2.30.tar.gz 
-export PREFIX="$HOME/opt/cross"
-export TARGET=aarch64-unknown-linux-gnueabi
-export PATH="$PREFIX/bin:$PATH"
-mkdir build-binutils
-cd build-binutils/
-../binutils-2.30/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror --enable-shared --disable-threads --enable-libmpx --with-system-zlib --enable-__cxa_atexit --disable-libunwind-exceptions --enable-clocale=gnu --disable-libstdcxx-pch --disable-libssp --enable-plugin --disable-linker-build-id --enable-lto --enable-install-libiberty --with-linker-hash-style=gnu --with-gnu-ld --enable-gnu-indirect-function --disable-multilib --disable-werror --enable-checking=release --enable-default-pie --enable-default-ssp --enable-gnu-unique-object
-make -j4
-make check
-make install
+rustup component add rust-src llvm-tools
+cargo install xargo cargo-binutils
 cd
 cd git
 git clone https://github.com/qemu/qemu.git
